d0bd6b66f5156da514d4f3c20914c967e1a2b45f
Matias <matiasquevedo.sabbath@gmail.com>
2018-05-17 18:07:34 -0300

    vista inicio

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -14,44 +14,12 @@
 							<h3 class="m-b-0">
 								<strong>Inicio</strong>
 							</h3>
-							<p class="text-muted">Tareas de adminitracion de Noticias</p>
+							<p class="text-muted">Tareas de adminitracion de AeroClub San Rafael</p>
 							<div class="panel-actions"></div>
 						</div>
 						
 					</div>
 				</div>
-
-
-				<div class="row row-padded homepage-grid row-bordered p-t text-center">
-    <a href=""><div class="col-sm-4">
-      <img class="homepage-grid-icon" src="//cdn.shopify.com/s/files/1/0691/5403/t/141/assets/components-icon.svg?16007593649882510692">
-      <h5><strong>Usuarios</strong></h5>
-      <p class="text-muted">Lista de Usuarios</p>
-    </div></a>
-	<a href="">
-		<div class="col-sm-4">
-      <img class="homepage-grid-icon" src="//cdn.shopify.com/s/files/1/0691/5403/t/141/assets/sliders-icon.svg?16007593649882510692">
-      <h5><strong>Configuraciones</strong></h5>
-      <p class="text-muted">Envio de noticias</p>
-    </div>
-		
-	</a>
-    
-  </div>
-
-  <div class="row row-padded homepage-grid row-bordered p-t text-center">
-    
-	<a href="https://console.firebase.google.com/project/suard-1e428/notification?hl=es-419">
-		<div class="col-sm-4">
-      <img class="homepage-grid-icon" src="//cdn.shopify.com/s/files/1/0691/5403/t/141/assets/wrenches-icon.svg?16007593649882510692">
-      <h5><strong>Envio de Notificaciones</strong></h5>
-    </div>
-		
-	</a>
-    
-  </div>
-
-
 
 
 			</div>
